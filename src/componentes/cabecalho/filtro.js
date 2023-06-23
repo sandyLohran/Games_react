4d26c3df427492ff9a9256a60e2831f27875881d
sandyLohran <sandylohran@gmail.com>
2023-06-23 17:44:27 -0300

    Melhorias no designer, add filtro por genero

--- src/componentes/cabecalho/filtro.js
+++ src/componentes/cabecalho/filtro.js
@@ -1,18 +1,33 @@
 import React from "react";
 import "./filtro.css";
-const Filtro = ({ filterValue, handleFilterChange }) => {
-  return (
-    <div >
 
-    <input
-      className="pesquisar"
-      type="text"
-      placeholder="Filtrar por título"
-      value={filterValue}
-      onChange={handleFilterChange}
-      />
+const Filtro = ({ filterValue, handleFilterChange, selectedGenre, handleGenreChange, genres }) => {
+    console.log("Filter Title",filterValue);
+    console.log("Genre Select:",selectedGenre);
+    return (
+      <div className="s ">
+        <input
+          className="pesquisar "
+          type="text"
+          placeholder="Filtrar por título"
+          value={filterValue}
+          onChange={handleFilterChange}
+        />
+<select className="filtro" value={selectedGenre} onChange={handleGenreChange}>
+  <option value="">Todos os gêneros</option>
+  
+  {genres.map((genre) => (
+    
+    <option value={genre} key={genre}>
+      {genre}
+    </option>
+  ))}
+</select>
+
       </div>
-  );
-};
+      
+    );
+    
+  };
 
 export default Filtro;
